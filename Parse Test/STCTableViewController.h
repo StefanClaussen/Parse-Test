2a1c9be1e0bfe392065668eacbeff89394a66462
StefenClausen <stefanclaussen@me.com>
2014-06-02 12:54:10 +0100

    Complete ending lecture 289

---
+++ Parse Test/STCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCTableViewController.h
+//  Parse Test
+//
+//  Created by Stefan Claussen on 02/06/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCTableViewController : UITableViewController
+
+@end
--- Parse Test/STCViewController.m
+++ Parse Test/STCViewController.m
@@ -5,6 +5,9 @@
 #import "STCViewController.h"
 
 @interface STCViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+
 
 @end
 
@@ -14,15 +17,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    [testObject setObject:@"bar" forKey:@"foo"];
-    [testObject save];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)saveUserButtonPressed:(UIButton *)sender
+{
+    PFObject *loginCredentials = [PFObject objectWithClassName:@"LoginCredentials"];
+    loginCredentials[@"name"] = self.usernameTextField.text;
+    loginCredentials[@"password"] = self.passwordTextField.text;
+    
+    [loginCredentials saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded)
+        {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Save" message:@"Your object saved" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+            [alertView show];
+            NSLog(@"Save successful");
+        }
+        else if (error){
+            NSLog(@"%@", error);
+        }
+    }];
 }
 
 @end
