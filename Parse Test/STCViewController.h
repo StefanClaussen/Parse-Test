f1266ab0a7043b588818806f194d234f57e2f1c9
StefenClausen <stefanclaussen@me.com>
2014-06-02 10:22:47 +0100

    Initial Commit

---
+++ Parse Test/STCViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCViewController.h
+//  Parse Test
+//
+//  Created by Stefan Claussen on 02/06/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCViewController : UIViewController
+
+@end
